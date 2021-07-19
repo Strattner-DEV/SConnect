26a778ae6a55bac4c00eb8b007fc9b61aa579f42
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-07-19 14:40:03 -0300

    update install

--- install.cmd
+++ install.cmd
@@ -1,7 +1,7 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install ultravnc -y
-choco install teamviewer -y
+choco install teamviewer.host -y
 choco install googlechrome -y
 choco install autoit -y
 choco install python -y
