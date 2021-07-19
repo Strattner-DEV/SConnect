8dd30ce775c731ae062269ffc8de9e6cfbb6f88a
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-07-19 16:57:04 -0300

    update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 IP_MACHINE = "192.168.2.2"
 
 # TODO Atualizar Paths
-FOLDER_PATH = "C:\\Users\\bruno.messias\\OneDrive - Strattner\\Documentos\\GitHub\\SConnect\\backup"
-OUTPUT_PATH = "C:\\Users\\bruno.messias\\OneDrive - Strattner\\Documentos\\GitHub\\SConnect\\output.txt"
-JSON_PATH = "C:\\Users\\bruno.messias\\OneDrive - Strattner\\Documentos\\GitHub\\SConnect\\data.json"
+FOLDER_PATH = "C:\\Users\\strattner\\Desktop\\BACKUP"
+SCRIPT_PATH = "C:\\Users\\strattner\\Desktop\\SConnect-0.1.2"
+
+#------------- Não Atualizar ------------------------
+OUTPUT_PATH = f"{SCRIPT_PATH}\\output.txt"
+JSON_PATH = f"{SCRIPT_PATH}\\data.json"
+#----------------------------------------------------
 
 # TODO Atualizar URL
 API_URL = "http://192.168.1.136:8098/api/SmartConnect/SaveLog"
